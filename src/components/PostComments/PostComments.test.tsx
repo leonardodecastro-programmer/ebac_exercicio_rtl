647c78f5b7ca80b1ecd7b103836b56870829e188
quickly <leonardo-programmer@proton.me>
2024-03-22 18:22:16 -0300

    Add: test

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+describe('Test for the PostComment component', () => {
+    it('Should render the component correctly', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Add two comments', () => {
+        render(<PostComment/>);
+
+        // Get textarea id
+        const textarea = screen.getByTestId('textarea-for-comments')
+
+        // Test first comment
+        fireEvent.change(textarea, {target: {value: 'First comment'}})
+        fireEvent.submit(screen.getByTestId('add-comment'))
+
+        // Test Second comment
+        fireEvent.change(textarea, {target: {value: 'Second comment'}})
+        fireEvent.submit(screen.getByTestId('add-comment'))
+
+        // Expected two comments in screen
+        expect(screen.getByText('First comment')).toBeInTheDocument()
+        expect(screen.getByText('Second comment')).toBeInTheDocument()
     });
 });
